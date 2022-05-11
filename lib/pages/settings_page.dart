44009aa93ce3e5c11abfdf0e3581c948d3b75013
zedrex <kabeer.adib.abrar@gmail.com>
2022-05-11 15:47:07 +0600

    Added radio buttons in Setting Page

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pomoduo/utils/constants.dart';
+import 'package:custom_radio_grouped_button/custom_radio_grouped_button.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -11,7 +13,72 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const <Widget>[Text("Settings")],
+      children: <Widget>[
+        const Text("Settings"),
+        const Padding(
+          padding: EdgeInsets.fromLTRB(0, 36, 0, 16.0),
+          child: Text("Focus Time"),
+        ),
+        CustomRadioButton(
+          elevation: 0,
+          absoluteZeroSpacing: false,
+          selectedColor: PomoduoColor.themeColor,
+          unSelectedColor: PomoduoColor.foregroundColor,
+          buttonTextStyle: const ButtonTextStyle(
+            selectedColor: Colors.white,
+            unSelectedColor: Colors.white60,
+          ),
+          enableShape: true,
+          enableButtonWrap: true,
+          buttonLables: const ["20 m", "25 m", "30 m", "35 m", "40 m", "45 m"],
+          buttonValues: const ["20", "25", "30", "35", "40", "45"],
+          radioButtonValue: (value) {
+            print(value);
+          },
+        ),
+        const Padding(
+          padding: EdgeInsets.fromLTRB(0, 36, 0, 16.0),
+          child: Text("Short Break Time"),
+        ),
+        CustomRadioButton(
+          elevation: 0,
+          absoluteZeroSpacing: false,
+          selectedColor: PomoduoColor.themeColor,
+          unSelectedColor: PomoduoColor.foregroundColor,
+          buttonTextStyle: const ButtonTextStyle(
+            selectedColor: Colors.white,
+            unSelectedColor: Colors.white60,
+          ),
+          enableShape: true,
+          enableButtonWrap: true,
+          buttonLables: const ["5 m", "10 m", "15 m"],
+          buttonValues: const ["5", "10", "15"],
+          radioButtonValue: (value) {
+            print(value);
+          },
+        ),
+        const Padding(
+          padding: EdgeInsets.fromLTRB(0, 36, 0, 16.0),
+          child: Text("Long Break Time"),
+        ),
+        CustomRadioButton(
+          elevation: 0,
+          absoluteZeroSpacing: false,
+          selectedColor: PomoduoColor.themeColor,
+          unSelectedColor: PomoduoColor.foregroundColor,
+          buttonTextStyle: const ButtonTextStyle(
+            selectedColor: Colors.white,
+            unSelectedColor: Colors.white60,
+          ),
+          enableShape: true,
+          enableButtonWrap: true,
+          buttonLables: const ["15 m", "20 m", "25 m"],
+          buttonValues: const ["15", "20", "25"],
+          radioButtonValue: (value) {
+            print(value);
+          },
+        ),
+      ],
     );
   }
 }
