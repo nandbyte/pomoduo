f162665711ee921e12363c191ab1c5fd829e8a13
zedrex <kabeer.adib.abrar@gmail.com>
2022-05-16 17:58:58 +0600

    Added new component to statistics page

--- lib/pages/statistics_page.dart
+++ lib/pages/statistics_page.dart
@@ -11,13 +11,83 @@ class StatisticsPage extends StatefulWidget {
 class _StatisticsPageState extends State<StatisticsPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const <Widget>[
-        Text(
-          "Statistics",
-          style: PomoduoStyle.pageTitleStyle,
-        )
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const <Widget>[
+          Center(
+            child: Text(
+              "Statistics",
+              style: PomoduoStyle.pageTitleStyle,
+            ),
+          ),
+          SizedBox(height: 36),
+          TotalStatistics(),
+        ],
+      ),
     );
   }
 }
+
+class TotalStatistics extends StatelessWidget {
+  const TotalStatistics({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(24.0),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: PomoduoColor.foregroundColor,
+        border: Border.all(
+          color: PomoduoColor.foregroundColor,
+        ),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(20),
+        ),
+      ),
+      child: Center(
+          child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              children: const [
+                Text("Total Sessions"),
+                Text(
+                  "34",
+                  style: TextStyle(fontSize: 36),
+                )
+              ],
+            ),
+          ),
+          const VerticalDivider(
+            thickness: 1,
+            indent: 20,
+            endIndent: 0,
+            color: Colors.grey,
+          ),
+          Expanded(
+            child: Column(
+              children: const [
+                Text("Total Duration"),
+                Text(
+                  "34",
+                  style: TextStyle(fontSize: 36),
+                ),
+              ],
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
+
+
+// Text(
+//           "Hello",
+//           style: TextStyle(
+//             color: Colors.white70,
+//             fontSize: 24,
+//           ),
+//         ),
